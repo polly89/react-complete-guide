699d61beb5076474fd3662afdb6f4c7216981d3e
Christina Woodhams <cwoodhams89@gmail.com>
2022-12-16 05:12:57 -0500

    Filter for year added through moving data from child-parent and through use of state.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card'
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
     
-    return <Card className='expenses'>
-        <ExpenseItem
+    const filterChangeHaldler = selectedYear =>{
+      console.log('Expenses.js')
+      setFilteredYear(selectedYear);
+    };
+    return (
+    <div>
+      <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHaldler}/>  
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}></ExpenseItem>
@@ -23,5 +32,7 @@ const Expenses = (props) => {
         amount={props.items[3].amount}
         date={props.items[3].date}></ExpenseItem>
     </Card>
+  </div>
+  );
 }
 export default Expenses;
